5a01248f2b797d0608d489654df23a336ce20d8a
madelinecodes <missy11217@gmail.com>
2023-08-02 21:44:41 +0100

    dont re-render background animation

--- components/Page.vue
+++ components/Page.vue
@@ -1,18 +1,10 @@
 <template>
-  <Head>
-    <link rel="icon" type="image/png" sizes="32x32" href="tangerine_full.png" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
-      rel="stylesheet"
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </Head>
-  <div class="container" id="snow">
+  <Default>
     <Nav></Nav>
     <div class="content">
       <slot></slot>
     </div>
-  </div>
+  </Default>
 </template>
 
 <style scoped>
@@ -30,29 +22,6 @@
   font-family: "Righteous";
   color: #f5ece5;
 }
-
-#snow {
-  background: none;
-  background-image: url("~/assets/img/tangerine-border.png"), url("~/assets/img/tangerine-grid-md.png"),
-    url("~/assets/img/tangerine-grid-sm.png");
-  height: 100vh;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100vw;
-  z-index: 1;
-  animation: snow 1000s linear infinite;
-  transform: translate(0, 0);
-}
-@keyframes snow {
-  0% {
-    background-position: 0px 0px, 0px 0px, 0px 0px;
-  }
-  100% {
-    background-position: 50000px 50000px, 10000px 20000px, -10000px 15000px;
-  }
-}
-
 @media only screen and (min-width: 600px) {
   .content {
     font-size: 21px;
@@ -63,4 +32,6 @@
 <script>
 import Nav from "@/components/Nav.vue";
 import css from "assets/css/app.css";
+import Default from "@/layouts/default.vue";
+
 </script>
---
+++ layouts/Default.vue
@@ -0,0 +1,38 @@
+<template>
+  <Head>
+    <link rel="icon" type="image/png" sizes="32x32" href="tangerine_full.png" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
+      rel="stylesheet"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
+  <div class="container" id="snow">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+#snow {
+  background: none;
+  background-image: url("~/assets/img/tangerine-border.png"),
+    url("~/assets/img/tangerine-grid-md.png"),
+    url("~/assets/img/tangerine-grid-sm.png");
+  height: 100vh;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  z-index: 1;
+  animation: snow 1000s linear infinite;
+  transform: translate(0, 0);
+}
+@keyframes snow {
+  0% {
+    background-position: 0px 0px, 0px 0px, 0px 0px;
+  }
+  100% {
+    background-position: 50000px 50000px, 10000px 20000px, -10000px 15000px;
+  }
+}
+</style>
